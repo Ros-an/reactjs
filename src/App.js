16b941ca5a0df847ac62d986f829b9a6755af05a
roshan <mahatoroshan1999@gmail.com>
2023-07-17 07:00:24 +0530

    add: props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Roc"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "local"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "ADOPT ME!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "koibhi_1",
+      animal: "dog",
+      breed: "local"
+    }),
+    React.createElement(Pet, {
+      name: "koibhi_2",
+      animal: "dog_1",
+      breed: "sehar_ka"
+    }),
+    React.createElement(Pet, {
+      name: "koibhi_3",
+      animal: "dog_2",
+      breed: "bazaar_ka"
+    }),
   ]);
 };
 const container = document.getElementById("root");
